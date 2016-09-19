35f693d9c876a73bbf22ae2f01e1eda0e870b216
fissional <fission@gmail.com>
2016-09-19 11:26:29 -0700

    adding all the options for post creation

--- FacebookPageManager/Views/CreatePost.xaml.cs
+++ FacebookPageManager/Views/CreatePost.xaml.cs
@@ -5,6 +5,15 @@ namespace gluontest
 {
 	public partial class CreatePost : ContentPage
 	{
+		DateTime previousDate = DateTime.Now;
+		private DateTime SelectedDate
+		{
+			get
+			{
+				return datePost.Date + timePost.Time;
+			}
+		}
+
 		public CreatePost()
 		{
 			InitializeComponent();
@@ -17,7 +26,10 @@ namespace gluontest
 
 		private async void btnCreatePost_Clicked(object sender, EventArgs e)
 		{
-			var newPostId = await App.Facebook.CreatePost(App.FacebookSettings.CurrentPage, editorPostBody.Text, true);
+			var newPostId = await App.Facebook.CreatePost(
+				App.FacebookSettings.CurrentPage, 
+				editorPostBody.Text, 
+				switchPublished.IsToggled, switchSpecifyDate.IsToggled ? (DateTime?)SelectedDate : null);
 
 			if (newPostId != null)
 			{
@@ -33,13 +45,50 @@ namespace gluontest
 
 		private void datePost_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
 		{
-			if (e.PropertyName != "Date")
+			if (e.PropertyName != "Date" && e.PropertyName != "Time")
 			{
 				return;
 			}
 
 			// indicate this post will be unpublished if the publication date is after today
-			labelUnpublished.IsVisible = ((DatePicker)sender).Date > DateTime.Now;
+			labelUnpublished.IsVisible = SelectedDate > DateTime.Now;
+			previousDate = SelectedDate;
+		}
+
+		private void switchDate_Toggled(object sender, ToggledEventArgs e)
+		{
+			datePost.IsEnabled = e.Value;
+			timePost.IsEnabled = e.Value;
+
+			if (e.Value)
+			{
+				// if the date pickers become enabled, display the previously selected date
+				previousDate = SelectedDate;
+				datePost.Date = previousDate.Date;
+				timePost.Time = previousDate - previousDate.Date;
+			}
+			else
+			{
+				// when they are disabled, show the current date/time
+				datePost.Date = DateTime.Now.Date;
+				timePost.Time = DateTime.Now - datePost.Date;
+			}
+		}
+
+		private void switchPublish_Toggled(object sender, ToggledEventArgs e)
+		{
+			if (switchSpecifyDate == null)
+			{
+				return;
+			}
+
+			// if this post is not published, disable the date picker
+			if (!e.Value)
+			{
+				switchSpecifyDate.IsToggled = false;
+				datePost.Date = previousDate.Date;
+				timePost.Time = previousDate - previousDate.Date;
+			}
 		}
 	}
 }
