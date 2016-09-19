c96b6953a02ea06e800104b6735b848da9ce3ce4
fissional <fission@gmail.com>
2016-09-19 10:25:56 -0700

    start of post details page

---
+++ FacebookPageManager/Views/PostDetails.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace gluontest
+{
+	public partial class PostDetails : ContentPage
+	{
+		FacebookPost m_post;
+
+		public PostDetails(FacebookPost post)
+		{
+			InitializeComponent();
+			m_post = post;
+			this.BindingContext = m_post;
+		}
+
+		private void btnBack_Clicked(object sender, EventArgs e)
+		{
+			App.NavigateOut();
+		}
+	}
+}
