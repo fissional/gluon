bc4a305411feb3e6b5ea7e966f1efe5fa457890d
fissional <fission@gmail.com>
2016-09-19 21:16:26 -0700

    android project

---
+++ gluontest.Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace gluontest.Android
+{
+	[Activity(Label = "gluontest.Android", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : FormsApplicationActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			Forms.Init(this, savedInstanceState);
+			LoadApplication(new gluontest.App());
+		}
+	}
+}
+
